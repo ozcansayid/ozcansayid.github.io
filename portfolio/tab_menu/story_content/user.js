2dc64d55e580bda153d9e372b60d7ac7046cdd77
Sayid Özcan <ozcansayid@gmail.com>
2024-12-09 00:52:49 +0300

    update

---
+++ portfolio/tab_menu/story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6YNUKBpkxkZ":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('5YRCi5r9XrX');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6ZxC50IjvKS';
+const pulseAmount = 0.07;
+const delay = 0;
+addToTimeline(
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
