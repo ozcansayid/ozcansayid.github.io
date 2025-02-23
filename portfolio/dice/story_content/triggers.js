03b6dc4648ddced85442d12fd7b20da252f2cc74
Sayid Özcan <ozcansayid@gmail.com>
2025-02-23 19:47:51 +0300

    update

---
+++ portfolio/dice/story_content/triggers.js
@@ -0,0 +1,37 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "68teRFkTqkp":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('60hNLSd96HW');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6L5JvjG9amE';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
