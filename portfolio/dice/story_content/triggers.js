120b784574cb3f974f214b4b3301ce6fd970585f
Sayid Özcan <ozcansayid@gmail.com>
2025-02-23 20:08:20 +0300

    update

--- portfolio/dice/story_content/triggers.js
+++ portfolio/dice/story_content/triggers.js
@@ -2,8 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "68teRFkTqkp":
+      case "6ic8SGhgz1d":
         Script1();
+        break;
+      case "6gybYAzc8PG":
+        Script2();
+        break;
+      case "6YkPDbP7Jxy":
+        Script3();
         break;
   }
 }
@@ -21,6 +27,44 @@ window.Script1 = function()
 const duration = 1000;
 const easing = 'ease-out';
 const id = '6L5JvjG9amE';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6WsP8ptqIaT');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5VgfbfUQPDH';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6WsP8ptqIaT');
+const duration = 2000;
+const easing = 'ease-out';
+const id = '5VgfbfUQPDH';
 const pulseAmount = 0.07;
 player.addForTriggers(
 id,
