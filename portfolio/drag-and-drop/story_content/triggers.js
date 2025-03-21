76937108fbdafa46d9c2451d8dace12953af700d
Sayid Özcan <ozcansayid@gmail.com>
2025-03-21 12:30:02 +0300

    update

---
+++ portfolio/drag-and-drop/story_content/triggers.js
@@ -0,0 +1,79 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5cIjEzs0fH0":
+        Script1();
+        break;
+      case "5fFrSyQOLSO":
+        Script2();
+        break;
+      case "6HUvXjlb6ka":
+        Script3();
+        break;
+      case "6VDkVd9CAug":
+        Script4();
+        break;
+      case "6Dfecem0HGW":
+        Script5();
+        break;
+      case "6lu7e05RcOx":
+        Script6();
+        break;
+      case "6Tj8kMIIUs3":
+        Script7();
+        break;
+      case "64lkByp7e8U":
+        Script8();
+        break;
+      case "5cLXIVByjft":
+        Script9();
+        break;
+      case "5mBKKRwFlYG":
+        Script10();
+        break;
+      case "6H9wyN5Ywbl":
+        Script11();
+        break;
+      case "5pMTL2wVlSQ":
+        Script12();
+        break;
+      case "6h2lzqXfT7o":
+        Script13();
+        break;
+      case "6VZevOopvlN":
+        Script14();
+        break;
+      case "5uUBVsOkRhU":
+        Script15();
+        break;
+      case "68EphJm5tll":
+        Script16();
+        break;
+      case "5pPHB0gbTma":
+        Script17();
+        break;
+      case "5fvkFpcJMqg":
+        Script18();
+        break;
+      case "5ehVfWosC5J":
+        Script19();
+        break;
+      case "6hsD6TUxFTK":
+        Script20();
+        break;
+      case "5pgVmGl0kDN":
+        Script21();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ portfolio/drag-and-drop/story_content/user.js
@@ -0,0 +1,392 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='balik']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script2 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='brokoli']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script3 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='elma']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script4 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kuruyemis']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script5 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='yogurt']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script6 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='cips']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script7 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kola']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script8 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='hamburger']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script9 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kizartma']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script10 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='sekerleme']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script11 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='balik']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script12 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='brokoli']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script13 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='elma']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script14 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kuruyemis']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script15 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='yogurt']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script16 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='cips']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script17 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kola']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script18 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='hamburger']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script19 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='kizartma']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script20 = function()
+{
+  // Storyline'da sürüklenen objeyi seçme
+var draggedItem = document.querySelector("[data-acc-text='sekerleme']");
+
+// Eğer obje varsa animasyonu başlat
+if (draggedItem) {
+    draggedItem.style.transition = "transform 1s ease-in-out, opacity 1s";
+    draggedItem.style.transform = "scale(0)";
+	draggedItem.style.opacity = "0";
+
+    // Belirli bir süre sonra tamamen gizle
+    setTimeout(function() {
+        draggedItem.style.display = "none";
+    }, 700);
+}
+}
+
+window.Script21 = function()
+{
+  const player = GetPlayer(); //Storyline Player nesnesine erişim sağladık
+
+let hedefpuan = player.GetVar("puan"); //Hedef puanı çağırdık Örn: 90
+let simdikipuan = 0; //Animasyonun başlangıç değeri
+
+const artishizi = 25; //Her adımda 25 milisaniye
+
+const adim = 1; //Puan artış sayısı
+
+let baslat = setInterval(() => { //animasyonu başlat
+
+	simdikipuan += adim; //puanı artır
+	
+	player.SetVar("puan", simdikipuan); //Puan değişkenimizi arttıkça güncelliyoruz
+	
+	if (simdikipuan >= hedefpuan) {
+		clearInterval(baslat) //hedef puana ulaşıldıysa animasyonu durdur
+	}
+}, artishizi) 
+}
+
+};
