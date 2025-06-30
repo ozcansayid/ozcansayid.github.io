df980b4900feceef6667378d2c321c058b1dcbb4
Sayid Özcan <ozcansayid@gmail.com>
2025-06-30 23:20:51 +0300

    update

---
+++ portfolio/karisik-kelimeler/story_content/triggers.js
@@ -0,0 +1,53 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5haQwMegOmy":
+        Script1();
+        break;
+      case "5r8fy0ZHvJN":
+        Script2();
+        break;
+      case "5WP5iEKJTqG":
+        Script3();
+        break;
+      case "5eGNrGaOkUR":
+        Script4();
+        break;
+      case "5VZlpTZ3xg6":
+        Script5();
+        break;
+      case "6hi5ok1onFn":
+        Script6();
+        break;
+      case "5b60f4eQ6n0":
+        Script7();
+        break;
+      case "5kGGMbZrw81":
+        Script8();
+        break;
+      case "61RHxaVHUDW":
+        Script9();
+        break;
+      case "5ccA8RUTqR2":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
---
+++ portfolio/karisik-kelimeler/story_content/user.js
@@ -0,0 +1,296 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script2 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script3 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script4 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script5 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script6 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script7 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script8 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script9 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+window.Script10 = function()
+{
+  var player = GetPlayer();
+
+// True/False değişkenleri
+player.SetVar("toggle_status", false);
+player.SetVar("müzik", true);
+player.SetVar("ileri_btn", false);
+player.SetVar("geri_btn", false);
+
+// Sayısal değişkenler
+player.SetVar("sayfa_kontrol", 0);
+player.SetVar("puan_artis", 2);  // bu default değeri zaten 2 olarak kalıyor
+player.SetVar("puan", 0);
+player.SetVar("kelime_sayisi", 0);
+player.SetVar("drag1_durum", 0);
+player.SetVar("drag2_durum", 0);
+player.SetVar("drag3_durum", 0);
+
+// (İsteğe bağlı) LocalStorage veya sessionStorage temizleme
+localStorage.clear();
+sessionStorage.clear();
+
+// Kursu baştan başlat (hard reload)
+window.location.reload(true);
+
+}
+
+};
