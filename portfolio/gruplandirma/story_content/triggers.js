48d52127a9a5643f0b8e6143da35f2b1d727e6ac
Sayid Özcan <ozcansayid@gmail.com>
2025-07-02 00:06:32 +0300

    update

---
+++ portfolio/gruplandirma/story_content/triggers.js
@@ -0,0 +1,35 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5qgMAprz7Z8":
+        Script1();
+        break;
+      case "5f59aRNwTJN":
+        Script2();
+        break;
+      case "69mJltYtocI":
+        Script3();
+        break;
+      case "6Tih9dQDUTQ":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
