e5e637975d40b5022dd8a5b627e6dea8d2dcd432
Sayid Özcan <ozcansayid@gmail.com>
2025-07-09 19:30:02 +0300

    update

---
+++ portfolio/dogruyu-bul/story_content/triggers.js
@@ -0,0 +1,53 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6KueuI5pJzR":
+        Script1();
+        break;
+      case "6BsVz7PNFSB":
+        Script2();
+        break;
+      case "6E00cfvrC20":
+        Script3();
+        break;
+      case "6lkWFxJKgVj":
+        Script4();
+        break;
+      case "6JMPgUMLuTt":
+        Script5();
+        break;
+      case "6UhnGXLAscg":
+        Script6();
+        break;
+      case "6csuK9A65on":
+        Script7();
+        break;
+      case "63f146jzJjP":
+        Script8();
+        break;
+      case "5ZB5j1fgAUk":
+        Script9();
+        break;
+      case "6cGXcIhHR1O":
+        Script10();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
