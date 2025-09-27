d9fb170b89168cda644e6bad71be18a514d991e7
Sayid Özcan <ozcansayid@gmail.com>
2025-09-27 09:37:59 +0300

    update

---
+++ portfolio/derslig/sablonlar/ilkokul-oyunlastirilmis/story_content/triggers.js
@@ -0,0 +1,134 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5xpTa6kpYL5":
+        Script1();
+        break;
+      case "6jXfJL5TG5B":
+        Script2();
+        break;
+      case "6kQ5E97nCr5":
+        Script3();
+        break;
+      case "6M5MrwpeLKn":
+        Script4();
+        break;
+      case "61MO2eVv0ns":
+        Script5();
+        break;
+      case "6Ha3QcYgFLA":
+        Script6();
+        break;
+      case "6UzesGzEaII":
+        Script7();
+        break;
+      case "5yorBcieLtm":
+        Script8();
+        break;
+      case "6mIA7fbAwP7":
+        Script9();
+        break;
+      case "5lQesdP7N99":
+        Script10();
+        break;
+      case "6HmH5J8UwgW":
+        Script11();
+        break;
+      case "6GP51P5pIxZ":
+        Script12();
+        break;
+      case "6A4yzXP6iGI":
+        Script13();
+        break;
+      case "5lLG0SpbyCF":
+        Script14();
+        break;
+      case "62kOukvkgBO":
+        Script15();
+        break;
+      case "5bYN4t6OUM7":
+        Script16();
+        break;
+      case "6SX23O0eGfA":
+        Script17();
+        break;
+      case "6ixe5P6aCeL":
+        Script18();
+        break;
+      case "5VgEc3AoZZK":
+        Script19();
+        break;
+      case "6kVa9L3WO0r":
+        Script20();
+        break;
+      case "6foWB3WcKzJ":
+        Script21();
+        break;
+      case "5kkOZt1Fr3P":
+        Script22();
+        break;
+      case "6UceNU4Wvqx":
+        Script23();
+        break;
+      case "5mTZzqQOIPo":
+        Script24();
+        break;
+      case "5nQ1Hiw9Mz9":
+        Script25();
+        break;
+      case "6qJntRg4PL8":
+        Script26();
+        break;
+      case "6TOdZHEd7Uc":
+        Script27();
+        break;
+      case "6O2s2DTlpFf":
+        Script28();
+        break;
+      case "5d6ikoa9Gcv":
+        Script29();
+        break;
+      case "6RetIw2YaJH":
+        Script30();
+        break;
+      case "64yDqcUPtm8":
+        Script31();
+        break;
+      case "5l0u6kXB6YL":
+        Script32();
+        break;
+      case "5iBg0oQjiYl":
+        Script33();
+        break;
+      case "5tYe9JC4IUO":
+        Script34();
+        break;
+      case "5dxPOZWiPRv":
+        Script35();
+        break;
+      case "6oGt5SQFPtN":
+        Script36();
+        break;
+      case "6Ej8nLw8GLx":
+        Script37();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
