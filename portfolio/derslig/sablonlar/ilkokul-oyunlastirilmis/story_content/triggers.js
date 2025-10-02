f5d09c8a24fef74035711acc236e03ff0c4b01b0
Sayid Özcan <ozcansayid@gmail.com>
2025-10-02 09:32:23 +0300

    update

--- portfolio/derslig/sablonlar/ilkokul-oyunlastirilmis/story_content/triggers.js
+++ portfolio/derslig/sablonlar/ilkokul-oyunlastirilmis/story_content/triggers.js
@@ -2,118 +2,118 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6HLAhTnfqkf":
+      case "6dAjXHqP33F":
         Script1();
         break;
-      case "5oOM0gTPqki":
+      case "6kVUqFBT09U":
         Script2();
         break;
-      case "6rPCCMpbRwB":
+      case "6nUr6JpJbbD":
         Script3();
         break;
-      case "6Z8mKCE5jhS":
+      case "6XvF79mtfbE":
         Script4();
         break;
-      case "65DCuEX5XAt":
+      case "6iPXTFB1MDX":
         Script5();
         break;
-      case "660hrYqWbOY":
+      case "6R4ifpI7XuW":
         Script6();
         break;
-      case "6qW5BZaMiGt":
+      case "5rvPngpIUcU":
         Script7();
         break;
-      case "5eN77n7pp7o":
+      case "6I1XzRsfHGE":
         Script8();
         break;
-      case "5n05kBOgH3t":
+      case "62vzx5xXoPa":
         Script9();
         break;
-      case "5ul9rIPcMf7":
+      case "6gMHNX9i8tI":
         Script10();
         break;
-      case "60PddhLcdeJ":
+      case "6MeRR5XdBn7":
         Script11();
         break;
-      case "6OrhPpPKLfc":
+      case "5xqLAgbcsBT":
         Script12();
         break;
-      case "6HHzBbfqSoH":
+      case "5sR2G5WfqFl":
         Script13();
         break;
-      case "6UjPJvA8AFH":
+      case "6b6FcCqCntt":
         Script14();
         break;
-      case "5zWyPXfkCVp":
+      case "5aajQhTOCoK":
         Script15();
         break;
-      case "6kvEQxoPOfY":
+      case "6hhOb9AG9Bq":
         Script16();
         break;
-      case "5gqa9kBiqY6":
+      case "6OON0lhLkW7":
         Script17();
         break;
-      case "69rr3q3F3w5":
+      case "607Vqgrpvh1":
         Script18();
         break;
-      case "5syECFGffPl":
+      case "5qvlDZqOcEk":
         Script19();
         break;
-      case "6WahoGTw7vf":
+      case "5bC4IFIIyMD":
         Script20();
         break;
-      case "5j3aGlwAPHC":
+      case "5XFamYsy8p8":
         Script21();
         break;
-      case "66W8rGmjMVo":
+      case "6puG9gX0MBS":
         Script22();
         break;
-      case "6ZTa92EpKC4":
+      case "692gyuW0Ha4":
         Script23();
         break;
-      case "5cMZKxKmDxs":
+      case "6Q06TyakaeT":
         Script24();
         break;
-      case "6ZUEsAFyGJ4":
+      case "6C85IcRhYYf":
         Script25();
         break;
-      case "6FTCVHTbtaR":
+      case "6VD95OteJsb":
         Script26();
         break;
-      case "5Ys5rgLoIb4":
+      case "61snEPa67MB":
         Script27();
         break;
-      case "6nDOLAvNE26":
+      case "6RW2TYnuFpn":
         Script28();
         break;
-      case "5i3eX9ZLLG6":
+      case "5nhFp08V3f0":
         Script29();
         break;
-      case "69mraOdRyWY":
+      case "6PY3Mszt8NK":
         Script30();
         break;
-      case "6hAUSnm3WqK":
+      case "66msXwvgLsj":
         Script31();
         break;
-      case "5tm0ANCoGvq":
+      case "5vxBjI3xcxO":
         Script32();
         break;
-      case "6LqGwLBMwdq":
+      case "5akGA39DK3S":
         Script33();
         break;
-      case "6md6ueEdoWk":
+      case "5qZgoVap3pw":
         Script34();
         break;
-      case "6MgK1lr0wdQ":
+      case "5pNcsPAC1bF":
         Script35();
         break;
-      case "5ciHPmqboX9":
+      case "5cYEscnuVZY":
         Script36();
         break;
-      case "6UL49VdPd1C":
+      case "6oSz5M0747U":
         Script37();
         break;
-      case "5plQdsnBwgu":
+      case "6SI9z09JnTl":
         Script38();
         break;
   }
