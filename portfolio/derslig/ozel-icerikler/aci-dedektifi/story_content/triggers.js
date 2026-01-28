47cda55c8670e0cf34a5bc845624c30aca3c3e6c
Sayid Özcan <ozcansayid@gmail.com>
2026-01-28 19:10:10 +0300

    update

---
+++ portfolio/derslig/ozel-icerikler/aci-dedektifi/story_content/triggers.js
@@ -0,0 +1,29 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6HbiU1f2W4G":
+        Script1();
+        break;
+      case "5q54K0cnwtr":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
